b4a307e4782f4333758527b5040fb6c85c90898c
harveengrewalll <hgrew4@uwo.ca>
2024-10-06 17:06:22 -0400

    Completed Tutorial

--- Tutorial_4.js
+++ Tutorial_4.js
@@ -3,20 +3,49 @@
 // B – big-endian (year, month, day), e.g. 2021-03-31
 // L – little-endian (day, month, year), e.g. 31-03-2021
 // M – middle-endian (month, day, year), e.g. 03-31-2021
-function getDateParser(format)
-{
+function getDateParser(format) {
     // Define a function that takes an arbitrariy number of strings containing dates to be parsed according to the given format
     // the date components are sperated by dashes (-)
     // returns a stringified array of objects containing the parsed dates
     // the order of the properties in each object should be day, month, year, regardless of the orginial format
-    
+    return function (...dateStrings) {
+        const parsedDates = [];
+
+        for (let dateString of dateStrings) {
+            let [first, second, third] = dateString.split("-");
+
+            let day, month, year;
+
+            if (format === 'B') {
+                year = first;
+                month = second;
+                day = third;
+            } else if (format === 'L') {
+                day = first;
+                month = second;
+                year = third;
+            } else if (format === 'M') {
+                month = first;
+                day = second;
+                year = third;
+            } else {
+                throw new Error("Invalid format! Please use 'B', 'L', or 'M'.")
+            }
+
+            parsedDates.push({
+                day: day,
+                month: month, 
+                year: year
+            });
+        }
+        return JSON.stringify(parsedDates)
+    }
 }
 
 // add an assignment for each of the following bindings so that it would be point at a closure of the corresponding format 
-let bigEndianParser ;
-let littleEndianParser;
-let middleEndianParser;
-
+let bigEndianParser = getDateParser('B');
+let littleEndianParser = getDateParser('L');
+let middleEndianParser = getDateParser('M');
 
 // Please don't change these lines
 module.exports.bigEndianParser = bigEndianParser;
